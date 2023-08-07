5da972cad921980b0dc0485ddd9aeb75d044b67f
Ma. Erika Santos <79189464+erikasnts22@users.noreply.github.com>
2023-08-07 00:12:13 -0400

    Update Exercise-2-Debugging-Practice.py

--- Day 1 - Working with Variables (String Manipulation)/Exercise-2-Debugging-Practice.py
+++ Day 1 - Working with Variables (String Manipulation)/Exercise-2-Debugging-Practice.py
@@ -1,3 +1,13 @@
+#Fix the code below
+
+print("Day 1 - String Manipulation"))
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.')
+
+
+#Debugged
+      
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
