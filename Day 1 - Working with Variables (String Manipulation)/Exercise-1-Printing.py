04ad15302b2a015585b33121fd463a2b5e28b314
Ma. Erika Santos <79189464+erikasnts22@users.noreply.github.com>
2023-08-07 00:09:06 -0400

    Update Exercise-1-Printing.py

--- Day 1 - Working with Variables (String Manipulation)/Exercise-1-Printing.py
+++ Day 1 - Working with Variables (String Manipulation)/Exercise-1-Printing.py
@@ -2,14 +2,18 @@ print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
+
 #New line
 print("Hi there!\nHow are you?")
+
 
 #Printing with space
 print("Hello" + " " + "Jack!")
 
+
 #User input
 input("What is your name? ")
+
 
 #Concatenated and nested input and output
 print("Hello " + input("What is your name? "))
