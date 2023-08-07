8816fc8491a9c4f2f5519f27c9a38613d909cc39
Ma. Erika Santos <79189464+erikasnts22@users.noreply.github.com>
2023-08-07 00:07:17 -0400

    Add files via upload

---
+++ Day 1 - Working with Variables (String Manipulation)/Exercise-3-Input-Function.py
@@ -0,0 +1,18 @@
+#Different ways to get the size of a String
+
+
+name = input("What is your name? ")
+print(len(name))
+
+
+name = "Jack"
+print(len(name))
+
+
+print(len(input("What is your name? ")))
+
+
+#Notes
+#If input was "Jack"
+#1st: print(len("Jack"))
+#2nd: print(4)
